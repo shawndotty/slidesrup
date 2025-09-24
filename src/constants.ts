315e60f445570901648b20b2bae356f180b79f14
johnny <shawndotty@gmail.com>
2025-09-24 10:17:20 +0800

    move airtable config to its own config file, and use git to manage constants

---
+++ src/constants.ts
@@ -0,0 +1,57 @@
+export const TEMPLATE_PLACE_HOLDERS = {
+	design: "DESIGN",
+	toc: "TOC",
+	presenter: "PRESENTER",
+	presentDate: "PRESENT_DATE",
+	tagline: "TAGLINE",
+	slogan: "SLOGAN",
+	pIndex: "PAGE_INDEX",
+	cIndex: "CHAPTER_INDEX",
+	cName: "CHAPTER_NAME",
+	baseLayout: "BASE_LAYOUT",
+	slidesRupPath: "SLIDESRUP_PATH",
+	slideName: "SLIDE_NAME",
+};
+
+export const DEFAULT_DESIGNS = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+export const SLIDESRUP_LIST_CLASSES = [
+	"box-list",
+	"fancy-list",
+	"fancy-list-row",
+	"fancy-list-with-order",
+	"fancy-list-with-order-row",
+	"grid-list",
+	"grid-step-list",
+	"grid-step-list-v",
+	"order-list-with-border",
+	"venn-list",
+	"pyramid-list",
+	"reverse-list-fragment",
+	"two-columns-list-1-2",
+	"two-columns-list-2-1",
+	"three-columns-list",
+	"three-columns-list-1-2-1",
+	"three-columns-list-1-1-2",
+	"three-columns-list-2-1-1",
+	"four-columns-list",
+];
+
+export const SLIDESRUP_COLUMN_CLASSES = [
+	"columns",
+	"twoCols-3-7",
+	"twoCols-7-3",
+	"twoCols-4-6",
+	"twoCols-6-4",
+	"threeCols-4-3-3",
+	"threeCols-3-4-3",
+	"threeCols-3-3-4",
+	"threeCols-l-5",
+	"threeCols-m-5",
+	"threeCols-r-5",
+	"fourCols-l-4",
+	"fourCols-r-4",
+];
+
+export const SLIDES_EXTENDED_PLUGIN_FOLDER = "plugins/slides-extended";
+export const ADVANCED_SLIDES_PLUGIN_FOLDER = "plugins/obsidian-advanced-slides";
